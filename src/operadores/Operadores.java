7dc8e44b4dd1b4f36904372ab877fa37f66f58fc
s109e10 <mi.a9786@hotmail.es>
2019-08-31 11:09:56 -0500

    Se agregó la clase operadores

--- src/operadores/Operadores.java
+++ src/operadores/Operadores.java
@@ -11,4 +11,40 @@ package operadores;
  */
 public class Operadores {
     
+    private static int numeroA;
+     private static int numeroB;
+     
+   public static void main(String [] args)
+   {
+       numeroA = 10;
+       numeroB = 5;
+       
+       //Operador módulo
+       int resultadoModulo = numeroA % 2;
+       System.out.println("El módulo de "+numeroA+" es "+resultadoModulo);
+       
+       //Operador incremento forma corta
+       int resultadoIncremento = numeroA++;
+       System.out.println("El resultado de incremento es "+resultadoIncremento);
+       
+           //Operador incremento forma larga
+       numeroA=numeroA+1;
+       System.out.println("El resultado de incremento es "+numeroA);
+       
+        //Operador decremento
+       numeroB--;
+       numeroB=numeroB-1;
+       int resultadoDecremento = numeroB++;
+       System.out.println("El resultado de incremento es "+resultadoDecremento);
+       
+       //Variable booleana tiene dos opciones (true, false)
+       boolean estaVivo = true;
+       estaVivo =  !estaVivo;
+       System.out.println("El valor de boolean es: "+estaVivo);
+       
+       
+       
+
+   }
+    
 }
